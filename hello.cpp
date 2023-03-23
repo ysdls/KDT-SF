e7b4b032d51d4f1c306344bfa8f48e9739c6953c
ysdls <ysdls84@gmail.com>
2023-03-23 17:23:54 +0900

    2day: variable

---
+++ hello.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "Hello World\n";
+
+    // int a = 1;
+    // int b = 2;
+    // std::cout << a + b;
+    // std::cout << "\n";
+
+    // double d = 1.2;
+    // double e = 3.9;
+    // std::cout << d + e << "\n";
+
+    bool c1 = true;
+    std::cout << c1 << "\n";
+    bool c2 = false;
+    std::cout << c2 << "\n";
+
+    char d = 'a';
+    char e = 'b';
+    std::cout << d << "\n"
+              << e << "\n";
+
+    std::string str1 = "hello";
+    std::string str2 = "world";
+    std::cout << str1 << "\n"
+              << str2;
+};
